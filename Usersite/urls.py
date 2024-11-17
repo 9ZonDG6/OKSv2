9effd0a8c526cff33dfa8879205741e75db1c9aa
9ZonD6 <32245323+9ZonDG6@users.noreply.github.com>
2024-11-18 00:15:05 +0500

    Переход на новую версию

--- Usersite/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import re_path
-from Usersite import views
-
-urlpatterns = [
-    re_path(r"^(?P<directory>[^/]+)/$", views.render_dynamic_page),
-]
--- Usersite/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-
-def render_dynamic_page(request, directory):
-    template_path = f"Usersite/{directory}/index.html"
-    return render(request, template_path)
